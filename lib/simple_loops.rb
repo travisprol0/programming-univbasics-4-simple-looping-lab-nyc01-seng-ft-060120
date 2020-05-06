8a831d28a69cb5351ece6b4910dc04ddce89bff0
Travis Prol <travisjohnprol@gmail.com>
2020-05-06 19:21:52 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -6,3 +6,12 @@ def loop_message_five_times(string)
     counter += 1
   end
 end
+
+def loop_message_n_times(string, num)
+  counter = 0
+
+  while counter <= num
+    puts string
+    counter += 1
+  end
+end
