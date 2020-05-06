4d5bfa49635f53109e578b8f0de4d9ac55db288c
Travis Prol <travisjohnprol@gmail.com>
2020-05-06 19:24:19 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -15,3 +15,12 @@ def loop_message_n_times(string, num)
     counter += 1
   end
 end
+
+def output_array(array)
+  counter = 0
+
+  while counter <= array.length
+    puts array[counter]
+    counter += 1
+  end
+end
