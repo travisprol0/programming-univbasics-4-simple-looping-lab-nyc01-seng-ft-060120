0ec4271e48c61d950f0e7643e3220260982540c0
Travis Prol <travisjohnprol@gmail.com>
2020-05-06 19:33:02 +0000

    Done.

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,37 @@
-# Write your methods here
+require 'pry'
+def loop_message_five_times(string)
+  counter = 0
+
+  while counter <= 5
+    puts string
+    counter += 1
+  end
+end
+
+def loop_message_n_times(string, num)
+  counter = 0
+
+  while counter <= num
+    puts string
+    counter += 1
+  end
+end
+
+def output_array(array)
+  counter = 0
+
+  while counter <= array.length
+    puts array[counter]
+    counter += 1
+  end
+end
+
+def return_string_array(array)
+  counter = 0
+
+  while counter < array.length
+    array[counter] = array[counter].to_s
+    counter += 1
+  end
+  array
+end
